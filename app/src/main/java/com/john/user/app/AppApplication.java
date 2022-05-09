688722f32a9a9410e5c79f98164a27eb5f6cc077
John Zhou <realjohnzhou@outlook.com>
2022-05-09 21:26:43 +0800

    feat: remove swagger annotation

--- app/src/main/java/com/john/user/app/AppApplication.java
+++ app/src/main/java/com/john/user/app/AppApplication.java
@@ -3,12 +3,10 @@ package com.john.user.app;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author john
  */
-@EnableSwagger2
 @MapperScan("com.john.user.app.mapper")
 @SpringBootApplication
 public class AppApplication {
