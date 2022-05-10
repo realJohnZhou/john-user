f8390af0f88ecb74b749865db2904d0f3321c942
John Zhou <realjohnzhou@outlook.com>
2022-05-10 18:33:41 +0800

    feat: add menu type enumeration

---
+++ app/src/main/java/com/john/user/app/enumeration/MenuTypeEnum.java
@@ -0,0 +1,15 @@
+package com.john.user.app.enumeration;
+
+/**
+ * @author john
+ */
+public enum MenuTypeEnum {
+    /**
+     * menu
+     */
+    MENU,
+    /**
+     * button
+     */
+    BUTTON
+}
