7572a5ebd3523b633b84f52a87c47f953cd48ea3
John Zhou <realjohnzhou@outlook.com>
2022-06-09 18:44:55 +0800

    feat: update PermissionServiceImpl

--- user-app/src/main/java/com/john/user/app/service/impl/PermissionServiceImpl.java
+++ user-app/src/main/java/com/john/user/app/service/impl/PermissionServiceImpl.java
@@ -1,11 +1,13 @@
 package com.john.user.app.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.john.user.app.entity.Permission;
 import com.john.user.app.mapper.PermissionMapper;
 import com.john.user.app.service.PermissionService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -16,5 +18,8 @@ public class PermissionServiceImpl extends ServiceImpl<PermissionMapper, Permiss
     @Override
     public void replace(String roleId, Set<String> menuIds) {
         // todo
+        QueryWrapper<Permission> queryWrapper = new QueryWrapper<>();
+        queryWrapper.lambda().eq(Permission::getRoleId, roleId);
+        List<Permission> permissions = this.baseMapper.selectList(queryWrapper);
     }
 }
