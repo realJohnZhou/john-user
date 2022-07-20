83dd7dc0b5e5a931533adb17ea5cbf2c9364fe11
John Zhou <realjohnzhou@outlook.com>
2022-07-20 19:44:43 +0800

    doc: add doc

--- user-app/src/main/java/com/john/user/app/mapper/PermissionMapper.java
+++ user-app/src/main/java/com/john/user/app/mapper/PermissionMapper.java
@@ -4,6 +4,8 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.john.user.app.entity.Permission;
 
 /**
+ * permission mapper
+ *
  * @author john
  */
 public interface PermissionMapper extends BaseMapper<Permission> {
